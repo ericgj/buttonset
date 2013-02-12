72133572ed53aee691ef31c2b2baba7285fb6002
Eric Gjertsen <ericgj72@gmail.com>
2013-02-12 18:04:32 -0500

    Remove jquery dependency
    - replace jquery DOM selection/manipulation with vanilla DOM methods
    - replace jquery ui binding with component/delegate binding
    - get button index via data- attributes
    - externalize button template (template-button.html)

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
  * Module dependencies.
  */
 
-var Emitter = require('emitter')
-  , classes = require('classes')
-  , inherit = require('inherit')
-  , o = require('jquery');
+var Emitter   = require('emitter')
+  , delegate  = require('delegate')
+  , classes   = require('classes')
+  , data      = require('data')
+  , domify    = require('domify')
+  , tmplBtn   = require('./template-button');
 
 /**
  * Expose `ButtonSet`.
@@ -18,13 +20,14 @@ module.exports = ButtonSet;
 /**
  * Initialize a new `ButtonSet`.
  *
- * @param {String|Object} el reference element
+ * @param {Element|String} el reference (container) element
  * @param {Object} opts options
  *
  *  - buttons {Array} initial buttons
  *  - unselectable {Boolean} allows unset the current selected option (default false)
  *  - multiple {Boolean} allows multiple selections (default false)
  *  - select {Number} allows select initial option
+ *  - indexattr {String} data- attribute for storing button index (default 'button-index')
  *
  * @api public
  */
@@ -33,9 +36,16 @@ function ButtonSet(el, opts) {
   if (!(this instanceof ButtonSet)) return new ButtonSet(el, opts);
   Emitter.call(this);
 
-  this.el = o(el);
-  classes(this.el.get(0)).add('buttonset');
+  this.el = (('string' == typeof el) ? document.querySelector(el) : el);
+  classes(this.el).add('buttonset');
   this.options = opts || {};
+  this.length = 0;
+
+  // force unselectable if buttonset is multiple
+  if (this.options.multiple) this.options.unselectable = true;
+
+  // set button index attribute or default
+  this.options.indexattr = this.options.indexattr || 'button-index';
 
   // add buttons
   if (this.options.buttons) {
@@ -44,13 +54,14 @@ function ButtonSet(el, opts) {
     }
   }
 
-  // force unselectable if buttonset is multiple
-  if (this.options.multiple) this.options.unselectable = true;
+  // bind click event from child buttons to onSet
+  delegate.bind(this.el, 
+                this.getButtonTemplate().nodeName, 'click', 
+                this.onSet.bind(this)
+               );
 
+  // set initial select index if given
   if ('undefined' != typeof this.options.select) this.set(this.options.select);
-
-  // bind click event to options
-  this.el.on('click', 'a', this.onSet.bind(this));
 
   return this;
 }
@@ -59,19 +70,23 @@ function ButtonSet(el, opts) {
  * Inherits from `Emitter.prototype`.
  */
 
-inherit(ButtonSet, Emitter);
+ButtonSet.prototype.__proto__ = Emitter.prototype;
 
 /**
- * Add a new option
+ * Add a new button option
  *
- * @param {String|jQuery} button button/buttons to add
+ * @param {String} button button/buttons to add
  * @api public
  */
 
 ButtonSet.prototype.add = function(){
   for (var i = 0; i < arguments.length; i++) {
-    this.el.append(o('<a href="#">' +  arguments[i] + '</a>'));
+    var b = this.getButtonTemplate().cloneNode(true);
+    b.innerHTML = arguments[i];
+    b.setAttribute('data-'+this.options.indexattr, this.length++);
+    this.el.appendChild(b);
   }
+  return this;
 };
 
 /**
@@ -84,14 +99,19 @@ ButtonSet.prototype.add = function(){
 ButtonSet.prototype.onSet = function(e){
   if (classes(e.target).has('selected')) {
     if (!this.options.unselectable) return;
-    return this.unset(o(e.target));
+    return this.unset(e.target);
   }
 
-  if (!this.options.multiple && this.el.find('.selected').length) {
-    this.unset(this.el.find('.selected'));
+  if (!this.options.multiple) {
+    var selected = this.el.querySelectorAll('.selected');
+    if (selected.length) {
+      for (var i=0; i<selected.length; i++) {
+        this.unset(selected[i]);
+      }
+    }
   }
 
-  this.set(o(e.target));
+  this.set(e.target);
 };
 
 /**
@@ -99,7 +119,7 @@ ButtonSet.prototype.onSet = function(e){
  *
  * Emits `set` (el, index) event
  *
- * @param {jQuery|Number} button option to select
+ * @param {Element|Number} button option to select
  * @api public
  */
 
@@ -112,7 +132,7 @@ ButtonSet.prototype.set = function(button){
  *
  * Emits `unset` (el, index) event
  *
- * @param {jQuery|Number} button option to select
+ * @param {Element|Number} button option to select
  * @api public
  */
 
@@ -123,16 +143,37 @@ ButtonSet.prototype.unset = function(button){
 /**
  * Set/Unset the given button
  *
- * @param {jQuery|Number} button option to select
+ * @param {Element|Number} button option to select
  * @param {Boolean} set defines set or unset button
  * @api private
  */
 
 ButtonSet.prototype.change = function(button, set){
-  button = 'number' == typeof button ? this.el.find('a').eq(button) : button;
-  if (!button.length) return false;
+  button = 'number' == typeof button ? this.el.children[button] : button;
+  var index = this.getButtonIndex(button);
 
-  classes(button.get(0))[set ? 'add' : 'remove']('selected');
-  this.emit(set ? 'set' : 'unset', button, button.prevAll().length);
+  classes(button)[set ? 'add' : 'remove']('selected');
+  this.emit(set ? 'set' : 'unset', button, this.getButtonIndex(button));
   return true;
 };
+
+/**
+ * Get button element from template (memoized)
+ *
+ * @api private
+ */
+
+ButtonSet.prototype.getButtonTemplate = function() {
+  return this.buttonTemplate || (this.buttonTemplate = domify(tmplBtn)[0]);
+}
+
+/**
+ * Get button index element from data- attribute
+ *
+ * @api public
+ */
+
+ButtonSet.prototype.getButtonIndex = function(button) {
+  return data(button).get(this.options.indexattr);
+}
+
---
+++ template-button.js
@@ -0,0 +1,1 @@
+module.exports = '<a href="#"></a>\n';

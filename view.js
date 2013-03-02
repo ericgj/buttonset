66ed8f47f3b9cb72776bba82f9ddec89904be1be
Eric Gjertsen <ericgj72@gmail.com>
2013-03-02 09:49:37 -0500

    vocab

--- view.js
+++ view.js
@@ -1,6 +1,6 @@
 require('./rivets-shim.js');
 
-var builder = require('rivets').bind
+var binder = require('rivets').bind
   , domify   = require('domify')
   , template = require('./template.js')
 
@@ -9,7 +9,7 @@ module.exports = ButtonSetView;
 function ButtonSetView(model){
   if (!(this instanceof ButtonSetView)) return new ButtonSetView(model);
   this.el = domify(template)[0];
-  builder(this.el, model);
+  binder(this.el, model);
 };
 
 
